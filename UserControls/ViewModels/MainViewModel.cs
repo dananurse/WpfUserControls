c6a2b694778e76055d0009fc6e62daa2555066b4
dana.nurse <dana.nurse@outlook.com>
2015-02-20 23:00:51 +0000

    Initial Commit

---
+++ UserControls/ViewModels/MainViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace UserControls.ViewModels
+{
+    public class MainViewModel
+    {
+        #region Commands
+
+        
+
+        #endregion
+
+        public MainViewModel()
+        {
+            InitiliseCommands();
+        }
+
+        private void InitiliseCommands()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
